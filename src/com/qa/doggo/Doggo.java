a0a2a3506a87275c3bc8fcf479ef777f9e1b8ea8
chrisRea <chris.j.rea1999@gmail.com>
2020-02-05 14:14:17 +0000

    Doggo

---
+++ src/com/qa/doggo/Doggo.java
@@ -0,0 +1,15 @@
+package com.qa.doggo;
+
+public class Doggo {
+	public static void main(String[] args) {
+
+		int doggo = 1;
+
+		for (int i = 0; i < 100; i++) {
+
+			System.out.println(i);
+
+		}
+
+	}
+}
